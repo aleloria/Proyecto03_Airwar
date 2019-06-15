13ae74b48bbaf8ecfa20ae832ab2c5d3af164446
aleloria <33795286+aleloria@users.noreply.github.com>
2019-06-15 12:15:09 -0600

    Se agregan aeropuertos y aviones segun la region

--- src/com/logic/Location.java
+++ src/com/logic/Location.java
@@ -11,15 +11,21 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 public class Location extends JPanel {
-	public Image Image = new ImageIcon("Images\\airport.png").getImage();
-	
+	public Image Image01 = new ImageIcon("Images\\airport.png").getImage();
+	public Image Image02 = new ImageIcon("Images\\plane.png").getImage();
 
-	public Location (int x, int y) {
+	public Location (int x, int y, int place) {
 		setBounds(x, y, 32, 32);
 		setLayout(new BorderLayout(0, 0));
 		setBackground(Color.gray);
 		JLabel l = new JLabel();
-		l.setIcon(new ImageIcon(Image));
+		if(place == 0) {
+			l.setIcon(new ImageIcon(Image01));
+		}
+		if(place == 1) {
+			l.setIcon(new ImageIcon(Image02));
+		}
+		
 		add(l, BorderLayout.CENTER);
 	}
 }
