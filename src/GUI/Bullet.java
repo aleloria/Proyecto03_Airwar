1a298c925d352843ae06e60c9b03dae9c06687d3
SMZ19 <sergio.197@hotmail.com>
2019-06-14 20:04:39 -0600

    Movimiento de AA
    Detecta las acciones del teclado

---
+++ src/GUI/Bullet.java
@@ -0,0 +1,36 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Bullet {
+    public Image Image = new ImageIcon("imagenes\\fireballD.gif").getImage().getScaledInstance(60,60,1);
+    public int posX,posY;
+
+    public Bullet(int x,int y){
+        //this.posX=x;
+        //this.posY=y;
+        setPosX(x);
+        setPosY(y);
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public java.awt.Image getImage() {
+        return Image;
+    }
+}
