c58ea6d01cc447f434b550b99a97d0aaf2bc0752
Dxnium <danielum99@gmail.com>
2019-06-15 23:22:03 -0600

    Mega merge

--- src/GUI/MovementGUI.java
+++ src/GUI/MovementGUI.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -8,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -26,16 +28,18 @@ public class MovementGUI extends JFrame implements KeyListener, ActionListener, 
 	int x= 10, y=400 , velX=0 , velYFire=0;
 	protected static Graphics2D dbg;
 	protected static Image dbImage, load;
+	ArrayList<Bullet> bulletList = new ArrayList<Bullet>();
 
 	AntiAircraft AA = new AntiAircraft();
-	Bullet bullet = new Bullet(0);
+	Bullet bullet = new Bullet();
 
 	Boolean attacking = false;
+	Boolean reachDestination = true;
 	//generator 
 	public SpotPos S_Pos[] = new SpotPos[19];
 	public int SpotType;
 	public Plane planes[];
-	
+
 	//plane animation
 	Timer tm = new Timer(10,this);
 
@@ -46,32 +50,34 @@ public class MovementGUI extends JFrame implements KeyListener, ActionListener, 
 		int keyCode = e.getKeyCode();
 
 		if (keyCode == e.VK_LEFT) {
-			setVelX(-4);
 
 		}
 		if (keyCode == e.VK_RIGHT) {
 
-			setVelX(+4);
+
 
 		}
 	}
+
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int keyCode = e.getKeyCode();
 		if (keyCode == e.VK_LEFT) {
 
-			setVelX(0);
 		}
 		if (keyCode == e.VK_RIGHT) {
 
-			setVelX(0);
+
 		}
 
 		if (keyCode == e.VK_SPACE ) {
-
-			attacking = true;
-			bullet.setPosX(AA.getPosX());
+			if(reachDestination) {
+				attacking = true;
+				bullet.setPosX(AA.getPosX());
+				Bullet bullets = new Bullet();
+				bulletList.add(bullets);
+			}
 		}
 
 	}
@@ -97,30 +103,27 @@ public class MovementGUI extends JFrame implements KeyListener, ActionListener, 
 
 	}
 	private void moving() throws InterruptedException {
-		if(AA.getPosX()>=0 && AA.getPosX()<=1100) {
-			AA.setPosX(AA.getPosX()+velX);
+		System.out.println(AA.getPosX());
+		if(AA.getPosX()==4) {
+			setVelX(4);
 
-		}
-		else{
-			if(AA.getPosX()<0) {
-
-				velX = 0;
-				AA.setPosX(0);
-			}
-			else if (AA.getPosX()>1100) {
-				velX = 0;
-				AA.setPosX(1100);
-			}
 
 		}
 
+		if (AA.getPosX()==1104) {
+
+			setVelX(-4);
+
+		}
+
+		AA.setPosX(AA.getPosX()+velX);
 
 	}
 	private void movingBullet() throws InterruptedException {
 		if(attacking) {
 			System.out.println(bullet.getPosY());
 			if (bullet.getPosY() > 5){
-
+				reachDestination = false;
 				velYFire -= 4;
 				bullet.setPosY(AA.getPosY()+velYFire);
 
@@ -128,8 +131,9 @@ public class MovementGUI extends JFrame implements KeyListener, ActionListener, 
 			else {
 
 				bullet.setPosY(AA.getPosY());
-				velYFire = 0;
+				velYFire =0;
 				attacking = false;
+				reachDestination = true;
 
 
 			}
@@ -154,10 +158,19 @@ public class MovementGUI extends JFrame implements KeyListener, ActionListener, 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-//		x = x + velX;
-//		p.setPosX(x + velX);
 		for(Plane x: planes){
-			x.setPosX(x.getPosX()+x.getVelX());
+			int[] posFinal = {S_Pos[x.getFinalVertex()].getposX(),S_Pos[x.getFinalVertex()].getposy()};
+			int posX = x.getPosX();
+			int posY = x.getPosY();
+			if(posX>posFinal[0]) {
+				x.setPosX(posX - x.getVelX());
+			}if(posX<posFinal[0]) {
+				x.setPosX(posX + x.getVelX());
+			}if(posY>posFinal[1]) {
+				x.setPosY(posY - x.getVelY());
+			}if(posY<posFinal[1]) {
+				x.setPosY(posY + x.getVelY());
+			}
 		}
 	}
 	public MovementGUI() {
@@ -211,9 +224,11 @@ public class MovementGUI extends JFrame implements KeyListener, ActionListener, 
 		g.drawRect(AA.getPosX()-1, AA.getPosY()-4, 167, 197);
 		g.drawImage(AA.getImageData(),AA.getPosX(), AA.getPosY(), null);
 		if(attacking) {
-			g.drawImage(bullet.getBulletImg(),bullet.getPosX(), AA.getPosY()+velYFire, null);
-			g.drawRect(bullet.getPosX()+18 , bullet.getPosY()+10, 21, 20);
-
+			for(int i =0;i<2;i++) {
+				g.drawImage(bullet.getBulletImg(),bullet.getPosX()+(i*100), AA.getPosY()+velYFire, null);
+				g.setColor(Color.WHITE);
+				g.drawRect(bullet.getPosX()+(i*100)+18 , bullet.getPosY()+10, 21, 20);
+			}
 		}
 		tm.start();
 		repaint();
