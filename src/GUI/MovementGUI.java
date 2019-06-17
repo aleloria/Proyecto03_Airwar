beae24cdfcad2addd1bc2c46f275adf5677ae794
Dxnium <danielum99@gmail.com>
2019-06-17 01:52:53 -0600

    merge

--- src/GUI/MovementGUI.java
+++ src/GUI/MovementGUI.java
@@ -216,14 +216,14 @@ public class MovementGUI extends JFrame implements KeyListener, ActionListener, 
 		locationGenerator(20);
 		graph.randomPathGenerator(this.S_Pos);
 		//lbl
-		planesGenerator(20);
+		planesGenerator(5);
 		Thread t=new Thread() {
 			public void run() {
 				//the following line will keep this app alive for 1000 seconds,
 				//waiting for events to occur and responding to them (printing incoming messages to console).
 				try {Thread.sleep(15);} catch (InterruptedException ie) {}
 			}
-		};
+		}; 
 		t.start();
 		System.out.println("Started");
 
